9a51235350d0d334d4429cda6dc01cd46c9b7004
Àlex Pérez <alexperezpujol@disroot.org>
2020-05-05 02:27:40 +0200

    Test sort version function

--- internal/versions/versions_test.go
+++ internal/versions/versions_test.go
@@ -8,9 +8,127 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hashicorp/go-version"
 	. "github.com/little-angry-clouds/kubernetes-binaries-managers/internal/helpers"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestSortVersions(t *testing.T) { // nolint: funlen
+	var flagtests = []struct {
+		testName       string
+		input          []string
+		output         []string
+		allReleases    bool
+		allVersions    bool
+		versionsLength int
+	}{
+		{
+			"stable releases",
+			[]string{
+				"1.17.5", "1.18.2", "1.16.9", "1.18.1", "1.18.0",
+				"1.15.11", "1.17.4", "1.16.8", "1.15.10", "1.16.7",
+				"1.17.3", "1.15.9", "1.17.2", "1.16.6", "1.16.0-alpha.1",
+				"1.3.0-alpha.3", "1.6.0-beta.3", "1.13.5", "1.14.0-beta.1",
+				"1.13.0-alpha.1", "1.7.10", "1.15.1", "1.10.0-beta.2", "1.17.0-alpha.2",
+				"1.7.13", "1.9.2", "1.8.7", "1.9.1", "1.7.12", "1.8.6",
+			},
+			[]string{
+				"1.18.2", "1.18.1", "1.18.0", "1.17.5", "1.17.4",
+				"1.17.3", "1.17.2", "1.16.9", "1.16.8", "1.16.7",
+				"1.16.6", "1.15.11", "1.15.10", "1.15.9", "1.15.1",
+				"1.13.5", "1.9.2", "1.9.1", "1.8.7", "1.8.6",
+			},
+			false,
+			false,
+			20,
+		},
+		{
+			"mixed releases",
+			[]string{
+				"1.17.5", "1.18.2", "1.16.9", "1.18.1", "1.18.0",
+				"1.15.11", "1.17.4", "1.16.8", "1.15.10", "1.16.7",
+				"1.17.3", "1.15.9", "1.17.2", "1.16.6", "1.16.0-alpha.1",
+				"1.3.0-alpha.3", "1.6.0-beta.3", "1.13.5", "1.14.0-beta.1",
+				"1.13.0-alpha.1", "1.7.10", "1.15.1", "1.10.0-beta.2", "1.17.0-alpha.2",
+				"1.7.13", "1.9.2", "1.8.7", "1.9.1", "1.7.12", "1.8.6",
+			},
+			[]string{
+				"1.18.2", "1.18.1", "1.18.0", "1.17.5", "1.17.4",
+				"1.17.3", "1.17.2", "1.17.0-alpha.2", "1.16.9", "1.16.8",
+				"1.16.7", "1.16.6", "1.16.0-alpha.1", "1.15.11", "1.15.10",
+				"1.15.9", "1.15.1", "1.14.0-beta.1", "1.13.5", "1.13.0-alpha.1",
+			},
+			true,
+			false,
+			20,
+		},
+		{
+			"all non mixed releases",
+			[]string{
+				"1.17.5", "1.18.2", "1.16.9", "1.18.1", "1.18.0",
+				"1.15.11", "1.17.4", "1.16.8", "1.15.10", "1.16.7",
+				"1.17.3", "1.15.9", "1.17.2", "1.16.6", "1.16.0-alpha.1",
+				"1.3.0-alpha.3", "1.6.0-beta.3", "1.13.5", "1.14.0-beta.1",
+				"1.13.0-alpha.1", "1.7.10", "1.15.1", "1.10.0-beta.2", "1.17.0-alpha.2",
+				"1.7.13", "1.9.2", "1.8.7", "1.9.1", "1.7.12", "1.8.6",
+			},
+			[]string{
+				"1.18.2", "1.18.1", "1.18.0", "1.17.5", "1.17.4",
+				"1.17.3", "1.17.2", "1.16.9", "1.16.8", "1.16.7",
+				"1.16.6", "1.15.11", "1.15.10", "1.15.9", "1.15.1",
+				"1.13.5", "1.9.2", "1.9.1", "1.8.7", "1.8.6",
+				"1.7.13", "1.7.12", "1.7.10",
+			},
+			false,
+			true,
+			23,
+		},
+		{
+			"all mixed releases",
+			[]string{
+				"1.17.5", "1.18.2", "1.16.9", "1.18.1", "1.18.0",
+				"1.15.11", "1.17.4", "1.16.8", "1.15.10", "1.16.7",
+				"1.17.3", "1.15.9", "1.17.2", "1.16.6", "1.16.0-alpha.1",
+				"1.3.0-alpha.3", "1.6.0-beta.3", "1.13.5", "1.14.0-beta.1",
+				"1.13.0-alpha.1", "1.7.10", "1.15.1", "1.10.0-beta.2", "1.17.0-alpha.2",
+				"1.7.13", "1.9.2", "1.8.7", "1.9.1", "1.7.12", "1.8.6",
+			},
+			[]string{
+				"1.18.2", "1.18.1", "1.18.0", "1.17.5", "1.17.4",
+				"1.17.3", "1.17.2", "1.17.0-alpha.2", "1.16.9", "1.16.8",
+				"1.16.7", "1.16.6", "1.16.0-alpha.1", "1.15.11", "1.15.10",
+				"1.15.9", "1.15.1", "1.14.0-beta.1", "1.13.5", "1.13.0-alpha.1",
+				"1.10.0-beta.2", "1.9.2", "1.9.1", "1.8.7", "1.8.6",
+				"1.7.13", "1.7.12", "1.7.10", "1.6.0-beta.3", "1.3.0-alpha.3",
+			},
+			true,
+			true,
+			30,
+		},
+	}
+
+	for _, tt := range flagtests {
+		tt := tt
+		t.Run(tt.testName, func(t *testing.T) {
+			expectedVersionsStr := tt.output
+			receivedVersionsStr := tt.input
+			receivedVersionsVrs := make([]*version.Version, len(tt.input))
+			for i, raw := range receivedVersionsStr {
+				v, _ := version.NewVersion(raw)
+				receivedVersionsVrs[i] = v
+			}
+			actualVersionsVrs, err := SortVersions(receivedVersionsVrs, tt.allReleases, tt.allVersions)
+			actualVersionsStr := make([]string, len(actualVersionsVrs))
+			for i, raw := range actualVersionsVrs {
+				v := fmt.Sprintf("%v", raw)
+				actualVersionsStr[i] = v
+			}
+			assert.Nil(t, err)
+			assert.Equal(t, expectedVersionsStr, actualVersionsStr)
+			assert.Equal(t, tt.versionsLength, len(actualVersionsStr))
+		})
+	}
+}
 
 func TestGetLastPage(t *testing.T) {
 	var flagtests = []struct {
